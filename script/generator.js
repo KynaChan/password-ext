cb641f669bdd0503284ce29ded49038e0a130461
KynaChan <92019943+KynaChan@users.noreply.github.com>
2022-02-09 21:38:07 +0000

    generator js

--- script/generator.js
+++ script/generator.js
@@ -2,6 +2,8 @@
 //document.body.style.backgroundColor = "yellow";
 //const newContent = "Hi there and greetings!";
 
+//const { stringify } = require("querystring");
+
 
 // var btn = "<button style='height:300px;width:300px;color:black;z-index:100;'>oerigwirngowirngowr</button>";
 // $("*:password").append(btn);
@@ -11,18 +13,97 @@
 
 function copyPwd() 
 {
-    var pwdInput = $("#generatePwd").val();
+    var pwdInput = $("#newPwd").val();
     $.trim(pwdInput);
     navigator.clipboard.writeText(pwdInput);
-    console.log("COPIED!!");
+    //console.log("COPIED!!");
 }
 
-function slider()
-{    
-    var value = $(".slider").val();
-    var result = $("#numb");
-    result.text(value);
+
+function checkSelection()
+{
+    let length = $(".slider").val();
+    let newPwd = "";
+    let lengthDivisor = 1;
+
+    let upperChars = $("#upper").is(":checked");
+        numberChars = $("#number").is(":checked"); 
+        symbolChars = $("#symbol").is(":checked");
+
+    if (upperChars) lengthDivisor++;
+    if (numberChars) lengthDivisor++;
+    if (symbolChars) lengthDivisor++;
+
+
+    for(i = 0; i < length; i++)
+    {
+        if (upperChars) { 
+            newPwd += getUpper();
+            upperChars--;
+        }
+        else if (numberChars) { 
+            newPwd += getNumbers(); 
+        }
+        else if (symbolChars) { 
+            newPwd += getSymbols(); 
+        } 
+        else { 
+            newPwd += getLower(); 
+        } 
+    }
+
+    //var fin = newPwd.slice(0,length);
+    return newPwd;
+    //console.log(newPwd);
 }
+
+function generator()
+{
+    var newPwd = checkSelection();
+        shufflePwd = shuffle(newPwd);
+    $("#newPwd").val(shufflePwd);
+}
+
+
+function getRandomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getLower()
+{
+    return String.fromCharCode(getRandomRange(97,122));
+}
+function getUpper()
+{
+    return String.fromCharCode(getRandomRange(65,90));
+}
+function getNumbers()
+{
+    return String.fromCharCode(getRandomRange(48,57));
+}
+function getSymbols()
+{
+    const symb = "!@#$%^&*()_+-=[]{};':\"|,.<>\/\\?"
+    //console.log(symb);
+    return symb[Math.floor(Math.random()*symb.length) | 0];
+}
+
+
+function shuffle(pwd)
+{
+    var pwdSplit = pwd.split("");
+        length = pwdSplit.length;
+    
+    for (i = length - 1; i > 0; i--)
+    {
+        var j = Math.floor(Math.random() * (i + 1));
+        var char = pwdSplit[i];
+        pwdSplit[i] = pwdSplit[j];
+        pwdSplit[j] = char;
+    }
+    return pwdSplit.join("");
+}
+
 
 
 
@@ -32,16 +113,37 @@ function slider()
 function OnGeneratorLoaded()
 {
 
-    $("#copyPwd").click(copyPwd);
-    $(".slider").on('input', slider);
+    $('.slider').on('input',function () {
+        var numb = $(this).val();
+      
+        $("#numbInput").val(numb);
+        $("#numb").text(numb);
+      });
+      $('#numbInput').on('input', function(){
+        var numb = $(this).val();
 
+        if (numb > 32) { $(this).val(32); }
+        else if (numb < 6) { $(this).val(6); } 
+
+        $('.slider').val($(this).val());
+        $("#numb").text($(this).val());
+      });
+    
+    $(".slider").mousemove(function(event){ 
+        $("#numb").css("margin-left",event.pageX)
+    })
+
+    // $(".slider").on('input', slider);
+     $("#generate").click(generator);
     
     $("#myappShowpwd").mousedown(function(){
-        $("#generatePwd").attr('type', "text");
+        $("#newPwd").attr('type', "text");
       });
       $("#myappShowpwd").mouseup(function(){
-          $("#generatePwd").attr('type', "password");
+          $("#newPwd").attr('type', "password");
       });
+
+      $("#copyPwd").click(copyPwd);
 
     console.log("generator LOADED");
 }
